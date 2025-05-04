943866ca9aa091077a9fea74e1b670142959ae21
sonusah1 <roshansah2267@gmail.com>
2025-05-04 14:58:35 +0530

    multermiddleware updated

--- backend/src/middlewares/multer.middlewares.js
+++ backend/src/middlewares/multer.middlewares.js
@@ -3,7 +3,7 @@ import path from 'path';
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // make sure this folder exists
+    cb(null, './public'); // make sure this folder exists
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
